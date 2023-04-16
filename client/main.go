cf592df36c13abbe8c47ac56c1693ffd9ccca2e8
Jerry <a0979682372@gmail.com>
2023-04-16 13:56:10 +0800

    v1.0.0

---
+++ client/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bufio"
+	//"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+
+	//"log"
+	"math/big"
+
+	"os"
+	"time"
+
+	"github.com/quic-go/quic-go"
+)
+
+const addr = "10.0.2.15:30000"
+
+var (
+	device  string = "enp0s9"
+	err     error
+	timeout time.Duration = 1 * time.Second
+	m_ch    chan []byte
+)
+
+func main() {
+	fmt.Println("hello")
+	m_ch = make(chan []byte, 256)
+	err := Client()
+	if err != nil {
+		fmt.Println("client err")
+		fmt.Println(err)
+	}
+
+}
+
+func Client() error {
+	tlsConf := &tls.Config{
+		InsecureSkipVerify: true,
+		NextProtos:         []string{"quic-echo-example"},
+	}
+	conn, err := quic.DialAddr(addr, tlsConf, &quic.Config{
+		KeepAlivePeriod: time.Minute * 5,
+		EnableDatagrams: true,
+	})
+	if err != nil {
+		fmt.Println("conn err")
+		return err
+	}
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Println("What message do you want to send?")
+		fmt.Print("message: ")
+		text, _ := reader.ReadString('\n')
+		sendData := []byte(text)
+		err = conn.SendMessage(sendData)
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
+
+// Setup a bare-bones TLS config for the server
+func generateTLSConfig() *tls.Config {
+	key, err := rsa.GenerateKey(rand.Reader, 1024)
+	if err != nil {
+		panic(err)
+	}
+	template := x509.Certificate{SerialNumber: big.NewInt(1)}
+	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
+	if err != nil {
+		panic(err)
+	}
+	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
+	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
+
+	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
+	if err != nil {
+		panic(err)
+	}
+	return &tls.Config{
+		Certificates: []tls.Certificate{tlsCert},
+		NextProtos:   []string{"quic-echo-example"},
+	}
+}
---
+++ server/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"math/big"
+	"time"
+
+	"github.com/quic-go/quic-go"
+)
+
+func main() {
+	fmt.Println("hello")
+	err := Server()
+	if err != nil {
+		fmt.Println("server err")
+		fmt.Println(err)
+	}
+
+}
+
+func Server() error {
+	const addr = "10.0.2.15:30000"
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*2000)
+	defer cancel()
+
+	listener, err := quic.ListenAddr(addr, generateTLSConfig(), &quic.Config{
+		KeepAlivePeriod: time.Minute * 5,
+		EnableDatagrams: true,
+	})
+	if err != nil {
+		return err
+	}
+
+	conn, err := listener.Accept(ctx)
+	if err != nil {
+		return err
+	}
+
+	for {
+		size := 256
+		buf := make([]byte, size)
+		buf, err = conn.ReceiveMessage()
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		fmt.Printf("Got: %s", buf)
+
+	}
+	return err
+
+}
+
+// Setup a bare-bones TLS config for the server
+func generateTLSConfig() *tls.Config {
+	key, err := rsa.GenerateKey(rand.Reader, 1024)
+	if err != nil {
+		panic(err)
+	}
+	template := x509.Certificate{SerialNumber: big.NewInt(1)}
+	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
+	if err != nil {
+		panic(err)
+	}
+	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
+	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
+
+	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
+	if err != nil {
+		panic(err)
+	}
+	return &tls.Config{
+		Certificates: []tls.Certificate{tlsCert},
+		NextProtos:   []string{"quic-echo-example"},
+	}
+}
